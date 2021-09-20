129f0a775a7dfbac38be41facfcecc8985a2c1f4
wdd <wdd@qq.com>
2021-09-20 15:48:31 +0800

    first commit

---
+++ src/main/java/com/wdd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.wdd.git;
+
+/**
+ * @author wdd
+ * @date 2021年09月20日
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hell,git-test");
+    }
+}
